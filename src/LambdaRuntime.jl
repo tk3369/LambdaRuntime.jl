f08a5d091d72634015b9e9406f057cd621615f9e
Tom Kwong <tk3369@gmail.com>
2021-01-03 18:07:08 +0000

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.13

---
+++ src/LambdaRuntime.jl
@@ -0,0 +1,5 @@
+module LambdaRuntime
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using LambdaRuntime
+using Test
+
+@testset "LambdaRuntime.jl" begin
+    # Write your tests here.
+end
